78d5c50faf7631f99be7a2b85afbe2742b9244cc
Kenta SUZUKI <suzuken326@gmail.com>
2015-06-19 16:18:35 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# metav
+
+collection of metasyntactic variable
+
+## usage
+
+	go get github.com/suzuken/metav
+	./metav
+
+## license
+
+MIT
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// return random metasyntactic variable
+func main() {
+	m := []string{
+		"foo",
+		"bar",
+		"baz",
+		"foobar",
+		"hoge",
+		"piyo",
+		"fuga",
+		"kuke",
+		"hogehoge",
+	}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	fmt.Println(m[r.Intn(len(m))])
+}
